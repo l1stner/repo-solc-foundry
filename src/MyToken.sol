9a0d082b34b7574e15faaef3f844ffb8d8f395c1
l1stner <learn.unacademy21@gmail.com>
2025-02-15 02:13:33 +0000

    m

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,3 +1,12 @@
 // SPDX-Licende-Identifier: MIT
 
-pragma solidity ^0.8.13
+pragma solidity ^0.8.13;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyTokenName","MTN"){}
+
+}
+
+
